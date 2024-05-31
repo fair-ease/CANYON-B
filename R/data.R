2e12a8fbde48d8ae0cfa9e42af078545b2bd815e
mjosse <marie.josse@ifremer.fr>
2024-05-31 11:53:44 +0200

    try to add the right data in package

---
+++ R/data.R
@@ -0,0 +1,55 @@
+#' AT variable
+#'
+#' A dataset containing the AT variable
+#'The variables are as follows:
+#'
+#' @format A data frame with AT variables:
+"wgts_AT"
+
+#' CT variable
+#'
+#' A dataset containing the CT variable
+#'The variables are as follows:
+#'
+#' @format A data frame with CT variables:
+"wgts_CT"
+
+#' pH variable
+#'
+#' A dataset containing the pH variable
+#'The variables are as follows:
+#'
+#' @format A data frame with pH variables:
+"wgts_pH"
+
+#' NO3 variable
+#'
+#' A dataset containing the NO3 variable
+#'The variables are as follows:
+#'
+#' @format A data frame with NO3 variables:
+"wgts_NO3"
+
+#' pCO2 variable
+#'
+#' A dataset containing the pCO2 variable
+#'The variables are as follows:
+#'
+#' @format A data frame with pCO2 variables:
+"wgts_pC02"
+
+#' PO4 variable
+#'
+#' A dataset containing the PO4 variable
+#'The variables are as follows:
+#'
+#' @format A data frame with PO4 variables:
+"wgts_PO4"
+
+#' SiOH4 variable
+#'
+#' A dataset containing the SiOH4 variable
+#'The variables are as follows:
+#'
+#' @format A data frame with SiOH4 variables:
+"wgts_SiOH4"
