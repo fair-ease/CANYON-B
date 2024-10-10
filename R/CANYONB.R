4b260f51a2e83e6ac36099bf76927ec135ed0081
Marie Jossé <84919248+Marie59@users.noreply.github.com>
2024-10-10 09:09:30 +0200

    fix add "

--- R/CANYONB.R
+++ R/CANYONB.R
@@ -148,7 +148,7 @@ CANYONB <- function(date,lat,lon,pres,temp,psal,doxy,param,epres,etemp,epsal,edo
   for (i in (1:noparams)){
     if(paramflag[i]){ # calculate only desired parameters
       # load weights et al. from file
-      inwgts=data(paste0(wgts_",paramnames[i]));
+      inwgts=data(paste0("wgts_",paramnames[i]));
 
       noparsets=ncol(inwgts)-1; # number of networks in committee
       # Input normalization
